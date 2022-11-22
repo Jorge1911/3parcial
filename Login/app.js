b4ee3492664ed497d2cf410b5245f30dbde55e93
Jorge.1911 <pollo.19xoco@gmail.com>
2022-11-22 10:47:48 -0600

    subi mis practicas

--- Login/app.js
+++ Login/app.js
@@ -1,3 +1,13 @@
 import {app} from './firebase.js'
 
 console.log(app);
+
+
+
+const btnCrearCuenta=document.querySelector("#btnCrearCuenta");
+btnCrearCuenta.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const email=document.querySelector("#crearEmail").value;
+    const password=document.querySelector("#crearPassword").value;
+    console.log(email,password);
+});
---
+++ Login/firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+    const firebaseConfig = {
+    apiKey: "AIzaSyBs1hwUd0PGs8Nt7SaS13fE_SdBW7L6dWM",
+    authDomain: "appfirebase-8d5e2.firebaseapp.com",
+    projectId: "appfirebase-8d5e2",
+    storageBucket: "appfirebase-8d5e2.appspot.com",
+    messagingSenderId: "18279952343",
+    appId: "1:18279952343:web:f0f1c7858de2cd3240e592"
+    };
+
+    // Initialize Firebase
+    export const app = initializeApp(firebaseConfig);
